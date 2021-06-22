a27b6377d7e9c3d05d4ea9db97e9cfe67711b57b
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-06-22 18:05:44 +0100

    Update Community Moderator - 2021-06-22 - development.bat

--- Community Moderator - 2021-06-22 - development.bat
+++ Community Moderator - 2021-06-22 - development.bat
@@ -1,14 +1,11 @@
 @echo off
 
-cls
-git clone --depth 1 https://github.com/microsoft/winget-pkgs.git
-
 :1
 cls
 set NUMBER=
 set /p NUMBER="Please enter the pull request number: "
 if "%NUMBER%" == "" goto :1
-cd "%USERPROFILE%/Desktop/winget-pkgs"
+cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
 git fetch --force upstream master
 git fetch --force upstream refs/pull/%NUMBER%/head pull/%NUMBER%
 git checkout --force pull/%NUMBER%
@@ -19,8 +16,8 @@ goto :2
 set PATH=
 set /p PATH="Please enter the path to the folder where the manifest file(s) is (are) located: "
 if "%PATH%" == "" goto :2
-winget validate --manifest "%USERPROFILE%/Desktop/winget-pkgs/%PATH%"
-winget install --manifest "%USERPROFILE%/Desktop/winget-pkgs/%PATH%"
+winget validate --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%PATH%"
+winget install --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%PATH%"
 git checkout --detach --force upstream/master
 git branch --delete --force pull/%NUMBER%
 pause
