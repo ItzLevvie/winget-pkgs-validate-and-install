8628e7cfc2586bf9dfea1ce1972d10056b1da370
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-06-22 17:32:30 +0100

    Create Community Moderator - 2021-06-22 - development.bat

---
+++ Community Moderator - 2021-06-22 - development.bat
@@ -0,0 +1,24 @@
+@echo off
+
+:1
+cls
+set NUMBER=
+set /p NUMBER="Please enter the pull request number: "
+if "%NUMBER%" == "" goto :1
+cd "%USERPROFILE%/Desktop/winget-pkgs"
+git fetch --force upstream master
+git fetch --force upstream refs/pull/%NUMBER%/head pull/%NUMBER%
+git checkout --force pull/%NUMBER%
+git diff --name-only upstream/master...pull/%NUMBER%
+goto :2
+
+:2
+set PATH=
+set /p PATH="Please enter the path to the folder where the manifest file(s) is (are) located: "
+if "%PATH%" == "" goto :2
+winget validate --manifest "%USERPROFILE%/Desktop/winget-pkgs/%PATH%"
+winget install --manifest "%USERPROFILE%/Desktop/winget-pkgs/%PATH%"
+git checkout --detach --force upstream/master
+git branch --delete --force pull/%NUMBER%
+pause
+goto :1
