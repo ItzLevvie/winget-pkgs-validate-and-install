57b2e851eabb64e6c472a2cad428bf43222389f3
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-06-22 18:13:38 +0100

    Update Community Moderators - 2021-06-22 - development.bat

--- Community Moderators - 2021-06-22 - development.bat
+++ Community Moderators - 2021-06-22 - development.bat
@@ -6,7 +6,6 @@ set NUMBER=
 set /p NUMBER="Please enter the pull request number: "
 if "%NUMBER%" == "" goto :1
 cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
-git fetch --force upstream master
 git fetch --force upstream refs/pull/%NUMBER%/head:pull/%NUMBER%
 git checkout --force pull/%NUMBER%
 git diff --name-only upstream/master...pull/%NUMBER%
