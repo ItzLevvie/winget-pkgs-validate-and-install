94e81e2018ddedef41a08faf95fea5fc9b04cb3c
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-06-22 18:26:46 +0100

    Update Community Moderators - 2021-06-22 - development.bat

--- Community Moderators - 2021-06-22 - development.bat
+++ Community Moderators - 2021-06-22 - development.bat
@@ -2,13 +2,13 @@
 
 :1
 cls
-set NUMBER=
-set /p NUMBER="Please enter the pull request number: "
-if "%NUMBER%" == "" goto :1
+set PULL_REQUEST_NUMBER=
+set /p PULL_REQUEST_NUMBER="Please enter the pull request number: "
+if "%PULL_REQUEST_NUMBER%" == "" goto :1
 cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
-git fetch --force upstream refs/pull/%NUMBER%/head:pull/%NUMBER%
-git checkout --force pull/%NUMBER%
-git diff --name-only upstream/master...pull/%NUMBER%
+git fetch --force upstream refs/pull/%NUMBER%/head:pull/%PULL_REQUEST_NUMBER%
+git checkout --force pull/%PULL_REQUEST_NUMBER%
+git diff --name-only upstream/master...pull/%PULL_REQUEST_NUMBER%
 goto :2
 
 :2
@@ -18,6 +18,6 @@ if "%PATH%" == "" goto :2
 winget validate --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%PATH%"
 winget install --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%PATH%"
 git checkout --detach --force upstream/master
-git branch --delete --force pull/%NUMBER%
+git branch --delete --force pull/%PULL_REQUEST_NUMBER%
 pause
 goto :1
