fea4aa58f815d5cc30497d9bcc1d483b0e6b373b
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-07 16:46:46 +0100

    Update Community Moderators - 2021-07-07 - development.bat

--- Community Moderators - 2021-07-07 - development.bat
+++ Community Moderators - 2021-07-07 - development.bat
@@ -13,7 +13,7 @@ set /p PR_NUMBER="Please enter the pull request number: "
 if "%PR_NUMBER%" == "" goto :1
 git fetch --force upstream master > nul 2>&1
 git fetch --force upstream refs/pull/%PR_NUMBER%/head:pull/%PR_NUMBER%
-if "%ERRORLEVEL%" == "128" goto :1
+if %ERRORLEVEL% == 128 goto :1
 git checkout --force pull/%PR_NUMBER%
 git diff --raw --no-renames --diff-filter=AM upstream/master...pull/%PR_NUMBER%
 cd "C:/Users/%USERNAME%/Desktop"
@@ -24,7 +24,7 @@ set FOLDER_PATH=
 set /p FOLDER_PATH="Please enter the path to the folder where the manifest file(s) is (are) located: "
 if "%FOLDER_PATH%" == "" goto :2
 winget validate --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%FOLDER_PATH%"
-if "%ERRORLEVEL%" == "-2147024893" goto :2
+if %ERRORLEVEL% == -2147024893 goto :2
 winget install --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%FOLDER_PATH%"
 cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
 git checkout --detach --force upstream/master
