3d86bd7ed36fadc2fe287c47c6c6c0c27de38373
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-07 17:43:18 +0100

    Update Community Moderators - 2021-07-07 - development.bat

--- Community Moderators - 2021-07-07 - development.bat
+++ Community Moderators - 2021-07-07 - development.bat
@@ -18,24 +18,36 @@ if %ERRORLEVEL% == 9009 (
 set PR_NUMBER=
 set /p PR_NUMBER="Please enter the pull request number: "
 if "%PR_NUMBER%" == "" goto :1
+
 git fetch --force upstream master > nul 2>&1
+
 git fetch --force upstream refs/pull/%PR_NUMBER%/head:pull/%PR_NUMBER%
 if %ERRORLEVEL% == 128 goto :1
-git checkout --force pull/%PR_NUMBER%
+
+git checkout --force pull/%PR_NUMBER% > nul 2>&1
 git diff --raw --no-renames --diff-filter=AM upstream/master...pull/%PR_NUMBER%
-cd "C:/Users/%USERNAME%/Desktop"
+
 goto :2
 
 :2
+cd "C:/Users/%USERNAME%/Desktop"
+
 set FOLDER_PATH=
 set /p FOLDER_PATH="Please enter the path to the folder where the manifest file(s) is (are) located: "
 if "%FOLDER_PATH%" == "" goto :2
+
 winget validate --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%FOLDER_PATH%"
 if %ERRORLEVEL% == -2147024893 goto :2
+
 winget install --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%FOLDER_PATH%"
+
 cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
-git checkout --detach --force upstream/master
-git branch --delete --force pull/%PR_NUMBER%
+
+git checkout --detach --force upstream/master > nul 2>&1
+git branch --delete --force pull/%PR_NUMBER% > nul 2>&1
+
 pause
+
 cls
+
 goto :1
