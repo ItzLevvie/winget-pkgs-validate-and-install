6010178fb5fb1759541e04ba75b0fefcca82081d
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-08 16:51:05 +0100

    Update Community Moderators - 2021-07-08 - development.bat

--- Community Moderators - 2021-07-08 - development.bat
+++ Community Moderators - 2021-07-08 - development.bat
@@ -5,6 +5,12 @@ if %ERRORLEVEL% == 9009 (
     echo Windows Package Manager is not installed.
     goto :EOF
 )
+(
+    echo {
+    echo     "network": { "downloader": "wininet" },
+    echo     "visual": { "progressBar": "rainbow" }
+    echo }
+) > "C:/Users/%USERNAME%/AppData/Local/Packages/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe/LocalState/settings.json"
 git --version > nul 2>&1
 if %ERRORLEVEL% == 9009 (
     echo Git is not installed.
@@ -15,12 +21,6 @@ if %ERRORLEVEL% == 1 (
     echo "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs" does not exist.
     goto :EOF
 )
-(
-    echo {
-    echo     "network": { "downloader": "wininet" },
-    echo     "visual": { "progressBar": "rainbow" }
-    echo }
-) > "C:/Users/%USERNAME%/AppData/Local/Packages/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe/LocalState/settings.json"
 
 :1
 set PR_NUMBER=
@@ -34,10 +34,10 @@ if %ERRORLEVEL% == 128 (
 )
 git checkout pull/%PR_NUMBER% > nul 2>&1
 git diff --name-only --diff-filter=d upstream/master...pull/%PR_NUMBER%
+cd "C:/Users/%USERNAME%/Desktop"
 goto :2
 
 :2
-cd "C:/Users/%USERNAME%/Desktop"
 set DIRECTORY_PATH=
 set /p DIRECTORY_PATH="Please enter the directory path to the manifest: "
 if "%DIRECTORY_PATH%" == "" goto :2
@@ -47,6 +47,7 @@ winget install --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%DIR
 cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
 git checkout --detach upstream/master > nul 2>&1
 git branch --delete --force pull/%PR_NUMBER% > nul 2>&1
+cd "C:/Users/%USERNAME%/Desktop"
 pause
 cls
 goto :1
