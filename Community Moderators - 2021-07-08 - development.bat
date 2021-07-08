a74f41dd5c6246c1ecbd2ea9aac5bad9cddf2aad
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-08 17:04:13 +0100

    Update Community Moderators - 2021-07-08 - development.bat

--- Community Moderators - 2021-07-08 - development.bat
+++ Community Moderators - 2021-07-08 - development.bat
@@ -1,5 +1,7 @@
 @echo off
 cls
+
+:1
 winget --version > nul 2>&1
 if %ERRORLEVEL% == 9009 (
     echo Windows Package Manager is not installed.
@@ -21,28 +23,29 @@ if %ERRORLEVEL% == 1 (
     echo "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs" does not exist.
     goto :EOF
 )
+goto :2
 
-:1
+:2
 set PR_NUMBER=
 set /p PR_NUMBER="Please enter the pull request number: "
-if "%PR_NUMBER%" == "" goto :1
-git fetch upstream master > nul 2>&1
+if "%PR_NUMBER%" == "" goto :2
+::git fetch upstream master > nul 2>&1
 git fetch upstream refs/pull/%PR_NUMBER%/head:pull/%PR_NUMBER% > nul 2>&1
 if %ERRORLEVEL% == 128 (
     echo "%PR_NUMBER%" does not exist.
-    goto :1
+    goto :2
 )
 git checkout pull/%PR_NUMBER% > nul 2>&1
 git diff --name-only --diff-filter=d upstream/master...pull/%PR_NUMBER%
 cd "C:/Users/%USERNAME%/Desktop"
-goto :2
+goto :3
 
-:2
+:3
 set DIRECTORY_PATH=
 set /p DIRECTORY_PATH="Please enter the directory path to the manifest: "
-if "%DIRECTORY_PATH%" == "" goto :2
+if "%DIRECTORY_PATH%" == "" goto :3
 winget validate --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%DIRECTORY_PATH%"
-if %ERRORLEVEL% == -2147024893 goto :2
+if %ERRORLEVEL% == -2147024893 goto :3
 winget install --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%DIRECTORY_PATH%"
 cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
 git checkout --detach upstream/master > nul 2>&1
