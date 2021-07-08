79fe3287f47b5aff156700e3996f38f215682b8c
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-08 17:16:12 +0100

    Update Community Moderators - 2021-07-08 - development.bat

--- Community Moderators - 2021-07-08 - development.bat
+++ Community Moderators - 2021-07-08 - development.bat
@@ -30,14 +30,14 @@ set PR_NUMBER=
 set /p PR_NUMBER="Please enter the pull request number: "
 if "%PR_NUMBER%" == "" goto :2
 ::git fetch upstream master > nul 2>&1
-git fetch upstream refs/pull/%PR_NUMBER%/head:pull/%PR_NUMBER% > nul 2>&1
+git -C "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs" fetch upstream refs/pull/%PR_NUMBER%/head:pull/%PR_NUMBER% > nul 2>&1
 if %ERRORLEVEL% == 128 (
     echo "%PR_NUMBER%" does not exist.
     goto :2
 )
-git checkout pull/%PR_NUMBER% > nul 2>&1
-git diff --name-only --diff-filter=d upstream/master...pull/%PR_NUMBER%
-cd "C:/Users/%USERNAME%/Desktop"
+git -C "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs" checkout pull/%PR_NUMBER% > nul 2>&1
+git -C "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs" diff --name-only --diff-filter=d upstream/master...pull/%PR_NUMBER%
+::cd "C:/Users/%USERNAME%/Desktop"
 goto :3
 
 :3
@@ -47,9 +47,9 @@ if "%DIRECTORY_PATH%" == "" goto :3
 winget validate --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%DIRECTORY_PATH%"
 if %ERRORLEVEL% == -2147024893 goto :3
 winget install --manifest "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs/%DIRECTORY_PATH%"
-cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
-git checkout --detach upstream/master > nul 2>&1
-git branch --delete --force pull/%PR_NUMBER% > nul 2>&1
+::cd "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs"
+git -C "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs" checkout --detach upstream/master > nul 2>&1
+git -C "C:/Users/%USERNAME%/Documents/GitHub/winget-pkgs" branch --delete --force pull/%PR_NUMBER% > nul 2>&1
 cd "C:/Users/%USERNAME%/Desktop"
 pause
 cls
