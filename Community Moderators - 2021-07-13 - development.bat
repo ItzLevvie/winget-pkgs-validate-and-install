25e6917e2abab4ea2135b2456b5d6ee8ca4aeaed
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-13 12:57:02 +0100

    Update and rename Community Moderators - 2021-07-12 - development.bat to Community Moderators - 2021-07-13 - development.bat

--- Community Moderators - 2021-07-13 - development.bat
+++ Community Moderators - 2021-07-13 - development.bat
@@ -2,9 +2,9 @@
 cls
 
 :1
-if "%PROCESSOR_ARCHITECTURE%" NEQ "AMD64" goto :EOF
 for /f "tokens=6 delims=. " %%a in ('ver') do set OS_BUILD=%%a
 if %OS_BUILD% LSS 19041 goto :EOF
+if "%PROCESSOR_ARCHITECTURE%" NEQ "AMD64" goto :EOF
 winget --version > nul 2>&1
 if %ERRORLEVEL% EQU 9009 (
     choice /n /m "Windows Package Manager is not installed. Would you like to install it (Y/N)?"
