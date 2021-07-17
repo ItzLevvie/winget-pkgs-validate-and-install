86b39a26461c6a2003fd49b6445c34aee54ec406
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-17 16:47:05 +0100

    Update Community Moderators - 2021-07-17 - experimental.bat

--- Community Moderators - 2021-07-17 - experimental.bat
+++ Community Moderators - 2021-07-17 - experimental.bat
@@ -62,25 +62,25 @@ goto :3
 
 :3
 for /f "tokens=1,2,3,4,5,6,7 delims=/" %%a in ('git -C %REPOSITORY_PATH% diff --name-only --diff-filter=d upstream/master...pull/%PR_NUMBER%') do (
-    set DIRECTORY_PATH_5="%%a/%%b/%%c/%%d/%%e"
-    set DIRECTORY_PATH_6="%%a/%%b/%%c/%%d/%%e/%%f"
-    set DIRECTORY_PATH_7="%%a/%%b/%%c/%%d/%%e/%%f/%%g"
+    set RELATIVE_PATH_5="%%a/%%b/%%c/%%d/%%e"
+    set RELATIVE_PATH_6="%%a/%%b/%%c/%%d/%%e/%%f"
+    set RELATIVE_PATH_7="%%a/%%b/%%c/%%d/%%e/%%f/%%g"
 )
-set DIRECTORY_PATH=%DIRECTORY_PATH_5%
-winget validate --manifest %REPOSITORY_PATH%/%DIRECTORY_PATH_5% > nul 2>&1
+set RELATIVE_PATH=%RELATIVE_PATH_5%
+winget validate --manifest %REPOSITORY_PATH%/%RELATIVE_PATH_5% > nul 2>&1
 if %ERRORLEVEL% EQU -1978335191 (
-    set DIRECTORY_PATH=%DIRECTORY_PATH_6%
-    winget validate --manifest %REPOSITORY_PATH%/%DIRECTORY_PATH_6% > nul 2>&1
+    set RELATIVE_PATH=%RELATIVE_PATH_6%
+    winget validate --manifest %REPOSITORY_PATH%/%RELATIVE_PATH_6% > nul 2>&1
 )
 if %ERRORLEVEL% EQU -1978335191 (
-    set DIRECTORY_PATH=%DIRECTORY_PATH_7%
-    winget validate --manifest %REPOSITORY_PATH%/%DIRECTORY_PATH_7% > nul 2>&1
+    set RELATIVE_PATH=%RELATIVE_PATH_7%
+    winget validate --manifest %REPOSITORY_PATH%/%RELATIVE_PATH_7% > nul 2>&1
 )
 goto :4
 
 :4
-winget validate --manifest %REPOSITORY_PATH%/%DIRECTORY_PATH%
-winget install --manifest %REPOSITORY_PATH%/%DIRECTORY_PATH%
+winget validate --manifest %REPOSITORY_PATH%/%RELATIVE_PATH%
+winget install --manifest %REPOSITORY_PATH%/%RELATIVE_PATH%
 goto :5
 
 :5
