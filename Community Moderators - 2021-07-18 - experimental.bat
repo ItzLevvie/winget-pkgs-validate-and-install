4a3a76c1e4738932280781f6b6f19f49563087fb
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-07-18 15:46:27 +0100

    Update Community Moderators - 2021-07-18 - experimental.bat

--- Community Moderators - 2021-07-18 - experimental.bat
+++ Community Moderators - 2021-07-18 - experimental.bat
@@ -15,8 +15,8 @@ if %ERRORLEVEL% EQU 1 (
     curl --location --url https://github.com/ItzLevvie/winget-pkgs-validate-and-install/releases/download/latest/Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx --output "C:/Users/%USERNAME%/Downloads/Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx" > nul 2>&1
     powershell Add-AppxPackage -Path "C:/Users/%USERNAME%/Downloads/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -DependencyPath "C:/Users/%USERNAME%/Downloads/Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx" > nul 2>&1
     path %PATH%;"C:/Users/%USERNAME%/AppData/Local/Microsoft/WindowsApps"
-    del "C:\Users\%USERNAME%\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" > nul 2>&1
-    del "C:\Users\%USERNAME%\Downloads\Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx" > nul 2>&1
+    del /f "C:\Users\%USERNAME%\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" > nul 2>&1
+    del /f "C:\Users\%USERNAME%\Downloads\Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx" > nul 2>&1
 )
 if %ERRORLEVEL% EQU 2 goto :EOF
 (
@@ -33,7 +33,7 @@ if %ERRORLEVEL% EQU 1 (
     echo:
     curl --location --url https://github.com/ItzLevvie/winget-pkgs-validate-and-install/releases/download/latest/Git.exe --output "C:/Users/%USERNAME%/Downloads/Git.exe" > nul 2>&1
     "C:/Users/%USERNAME%/Downloads/Git.exe" /verysilent
-    del "C:\Users\%USERNAME%\Downloads\Git.exe" > nul 2>&1
+    del /f "C:\Users\%USERNAME%\Downloads\Git.exe" > nul 2>&1
     path %PATH%;"C:/Program Files/Git/cmd"
 )
 if %ERRORLEVEL% EQU 2 goto :EOF
