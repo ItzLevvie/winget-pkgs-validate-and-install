b7bb81c481a7baae981f9814b330363a15b7812b
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2021-12-20 13:18:07 +0000

    Some changes
    1) Simplify PATH variable a bit
    2) Use ntoskrnl.exe's to get the proper current build number: https://github.com/ocalvo/MyPowerShell/pull/1. Relying on Registry or deprecated APIs are unsupported.

--- Community Moderators - 2021-12-20 - development.ps1
+++ Community Moderators - 2021-12-20 - development.ps1
@@ -6,12 +6,12 @@ $ProgressPreference = "SilentlyContinue"
 function Initialize-PSSession {
     Clear-Host
     [System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-    $env:Path = $env:SystemRoot + "\System32" + ";" + $env:SystemRoot + "\System32\WindowsPowerShell\v1.0" + ";" + $env:LOCALAPPDATA + "\Microsoft\WindowsApps" + ";" + $env:ProgramFiles + "\Git\cmd"
+    $env:Path = "$env:SystemRoot\System32" + ";" + "$env:SystemRoot\System32\WindowsPowerShell\v1.0" + ";" + "$env:LOCALAPPDATA\Microsoft\WindowsApps" + ";" + "$env:ProgramFiles\Git\cmd"
     Get-WindowsOSBuild
 }
 
 function Get-WindowsOSBuild {
-    $OS_BUILD = (Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name CurrentBuild)
+    $OS_BUILD = (Get-ItemProperty -Path $env:SystemRoot\System32\ntoskrnl.exe).VersionInfo.ProductBuildPart
     if ($OS_BUILD -lt 19041) {
         Write-Host "This script requires Windows 10 version 20H1 or later." -ForegroundColor Red
         Write-Host
