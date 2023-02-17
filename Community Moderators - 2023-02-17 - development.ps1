7d69a1d283b2bd9e4eb7a34a5f2d39bf5e1efebf
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2023-02-17 14:42:27 +0000

    Force upgrade Git for Windows if older than 2.39.1

--- Community Moderators - 2023-02-17 - development.ps1
+++ Community Moderators - 2023-02-17 - development.ps1
@@ -73,7 +73,7 @@ function Initialize-WinGetSoftware2 {
 }
 
 function Initialize-GitSoftware {
-    if (-not(Get-Command -Name git) -or (git version).TrimStart("git version").Split(".")[0] + "." + (git version).TrimStart("git version").Split(".")[1] + "." + (git version).TrimStart("git version").Split(".")[2] -lt "2.37.3") {
+    if (-not(Get-Command -Name git) -or (git version).TrimStart("git version").Split(".")[0] + "." + (git version).TrimStart("git version").Split(".")[1] + "." + (git version).TrimStart("git version").Split(".")[2] -lt "2.39.1") {
         Write-Host "Downloading Git..."
         if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
             Invoke-WebRequest -Uri https://github.com/ItzLevvie/winget-pkgs-validate-and-install/releases/download/20230217.1/Git-prerelease-64-bit.exe -OutFile $env:TEMP\Git-prerelease.exe
