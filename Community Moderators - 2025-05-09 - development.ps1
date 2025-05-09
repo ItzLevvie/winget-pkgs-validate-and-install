1d920e55b7a90e44cb29b42393c4c8fcb3142fe6
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2025-05-09 15:29:38 +0100

    Update Community Moderators - 2025-05-09 - development.ps1

--- Community Moderators - 2025-05-09 - development.ps1
+++ Community Moderators - 2025-05-09 - development.ps1
@@ -200,7 +200,6 @@ function Read-PR {
         Request-PR
     }
     git -C $REPOSITORY_DIRECTORY sparse-checkout set $PACKAGE_VERSION_DIRECTORY
-    #git -C $REPOSITORY_DIRECTORY pull --no-edit --force upstream refs/pull/$PR_NUMBER/head
     git -C $REPOSITORY_DIRECTORY merge --no-edit FETCH_HEAD
     if ($LASTEXITCODE -eq 1) {
         Write-Host
