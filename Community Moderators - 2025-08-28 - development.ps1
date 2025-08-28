e280019052a73dd5008c54149280385e509f4fe2
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2025-08-28 14:46:14 +0100

    Update Community Moderators - 2025-08-28 - development.ps1

--- Community Moderators - 2025-08-28 - development.ps1
+++ Community Moderators - 2025-08-28 - development.ps1
@@ -71,7 +71,7 @@ function Set-WindowsSettings {
 function Initialize-WinGet {
     [System.Boolean]$WINGET_COMMAND = Test-Path -Path $env:LOCALAPPDATA\Microsoft\WindowsApps\winget.exe -PathType Leaf
     [System.Version]$WINGET_VERSION_CURRENT = (winget --version).TrimStart("v").TrimEnd("-preview")
-    [System.Version]$WINGET_VERSION_MINIMUM = "1.11.430"
+    [System.Version]$WINGET_VERSION_MINIMUM = "1.12.100"
     if (-not($WINGET_COMMAND) -or $WINGET_VERSION_CURRENT -lt $WINGET_VERSION_MINIMUM) {
         Write-Host "Downloading WinGet..."
         if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
