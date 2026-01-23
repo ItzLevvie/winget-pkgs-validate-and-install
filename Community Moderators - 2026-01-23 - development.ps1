0ad5b0d512be77b95de410148e98470a24db3600
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2026-01-23 20:33:01 +0000

    Update settings.json to 20260123.1

--- Community Moderators - 2026-01-23 - development.ps1
+++ Community Moderators - 2026-01-23 - development.ps1
@@ -108,7 +108,7 @@ function Set-WinGetSettings {
     [System.String]$SID_REQUIRED = "S-1-5-32-544"
     if (-not($WINGET_SETTINGS)) {
         if ($SID_CURRENT -eq $SID_REQUIRED) {
-            Invoke-WebRequest -Uri https://github.com/ItzLevvie/winget-pkgs-validate-and-install/releases/download/20251002.1/settings.json -OutFile $env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json
+            Invoke-WebRequest -Uri https://github.com/ItzLevvie/winget-pkgs-validate-and-install/releases/download/20260123.1/settings.json -OutFile $env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json
             winget settings --enable LocalManifestFiles
             winget settings --enable BypassCertificatePinningForMicrosoftStore
             winget settings --enable InstallerHashOverride
