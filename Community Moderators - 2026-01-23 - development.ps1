fee31b8a272fcee7b8a9163800128e0d363da7d7
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2026-01-23 17:42:01 +0000

    Update Community Moderators - 2026-01-23 - development.ps1

--- Community Moderators - 2026-01-23 - development.ps1
+++ Community Moderators - 2026-01-23 - development.ps1
@@ -1,4 +1,4 @@
-# This script requires you to run: Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine -Force ; & ".\Community Moderators - 2026-01-14 - development.ps1"
+# This script requires you to run: Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine -Force ; & ".\Community Moderators - 2026-01-23 - development.ps1"
 # in PowerShell 5.1 or later.
 
 $ErrorActionPreference = "SilentlyContinue"
